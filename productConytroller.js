f225c4c837cf4248ac3ba5141c425201e5dce2be
Hargun Badhesha <162474291+hargunbadhesha@users.noreply.github.com>
2024-08-29 09:31:57 -0400

    Create productConytroller.js

---
+++ productConytroller.js
@@ -0,0 +1,29 @@
+const Product = require('../models/productModel');
+
+// Get all products
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Create new product
+exports.createProduct = async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: req.body.image,
+        stock: req.body.stock
+    });
+
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
